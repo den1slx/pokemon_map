22a08a8c6d335d2510bfb5b0a1300c795e6486d8
den1slx <keycoingold@mail.ru>
2023-04-14 00:23:31 +1000

    Show Pokemon name in admin UI.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,3 +5,7 @@ class Pokemon(models.Model):
     title = models.CharField(max_length=200, blank=True)
     pokedex_num = models.IntegerField(default=0)
 
+    def __str__(self):
+        if self.pokedex_num > 0:
+            return f'№{self.pokedex_num}  {self.title}'
+        return self.title
