81cb2074cd96458282c1e58b20e3c94b01f0e98c
den1slx <keycoingold@mail.ru>
2023-04-14 02:41:08 +1000

    Add Pokemon.image model field

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,6 +4,7 @@ from django.db import models  # noqa F401
 class Pokemon(models.Model):
     title = models.CharField(max_length=200, blank=True, unique=True)
     pokedex_num = models.IntegerField(blank=True, null=True)
+    poke_image = models.ImageField(blank=True, null=True)
 
     def __str__(self):
         if self.pokedex_num and self.pokedex_num > 0:
