f1fdaf1fa56f9a31a421ccd38e627a0332f6aeb1
den1slx <keycoingold@mail.ru>
2023-04-14 02:33:33 +1000

    Update

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,10 +2,13 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200, blank=True)
-    pokedex_num = models.IntegerField(default=0)
+    title = models.CharField(max_length=200, blank=True, unique=True)
+    pokedex_num = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
-        if self.pokedex_num > 0:
-            return f'№{self.pokedex_num}  {self.title}'
-        return self.title
+        if self.pokedex_num and self.pokedex_num > 0:
+            return f'№{self.pokedex_num} |  {self.title}'
+
+        if self.pokedex_num == 0:
+            return f'№0 |  {self.title}'
+        return f'???? |  {self.title}'
