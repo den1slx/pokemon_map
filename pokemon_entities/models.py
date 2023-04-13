08ba4919d3b9c375d73254215639fa2435b2c9a8
den1slx <keycoingold@mail.ru>
2023-04-14 00:11:55 +1000

    Change model (test)

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,4 +3,5 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200, blank=True)
+    pokedex_num = models.IntegerField(default=0)
 
