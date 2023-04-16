5c4b4f9b8aa535a3447e1af4041920abbba44e69
den1slx <keycoingold@mail.ru>
2023-04-16 22:15:59 +1000

    Rework models

--- pokemon_entities/admin.py
+++ pokemon_entities/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Pokemon, PokemonEntity
+from .models import Pokemon, PokemonEntity, Evolution
 
 admin.site.register(Pokemon)
 admin.site.register(PokemonEntity)
+admin.site.register(Evolution)
--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,22 +2,25 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
-    title = models.CharField(max_length=200, blank=True, unique=True)
-    pokedex_num = models.IntegerField(blank=True, null=True, unique=True)
+    title_en = models.CharField(max_length=200, blank=True, unique=True)
+    title_ru = models.CharField(max_length=200, blank=True, null=True)
+    title_jp = models.CharField(max_length=200, blank=True, null=True)
+    pokedex_num = models.IntegerField(null=True, unique=True)
+    description = models.TextField(blank=True, null=True)
     poke_image_local = models.ImageField(blank=True, null=True)
     poke_image_link = models.URLField(blank=True, null=True)
 
     def __str__(self):
         if self.pokedex_num and self.pokedex_num > 0:
-            return f'№{self.pokedex_num} |  {self.title}'
+            return f'№{self.pokedex_num} |  {self.title_en}'
 
-        return f'№???? |  {self.title}'
+        return f'№???? |  {self.title_en}'
 
 
 class PokemonEntity(models.Model):
-    title = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    Lat = models.FloatField(default=0.0)
-    Lon = models.FloatField(default=0.0)
+    pokedex_num = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    Lat = models.FloatField(default=55.5)
+    Lon = models.FloatField(default=37.7)
     appeared_at = models.DateTimeField(null=True, blank=True)
     disappeared_at = models.DateTimeField(null=True, blank=True)
     catching_start = models.TimeField(null=True, blank=True)
@@ -30,11 +33,21 @@ class PokemonEntity(models.Model):
 
     def __str__(self):
         if self.catching_start == self.catching_end is not None:
-            return f'''Catching time: Anytime | Coords: 
-{self.Lat}:{self.Lon} | Pokemon:{self.title} |'''  # end string
+            return f'''| Pokemon:{self.pokedex_num} |'''  # end string
         elif self.catching_start == self.catching_end:
-            return f'''Catching time: Unknown | Coords: 
-{self.Lat}:{self.Lon} | Pokemon:{self.title} |'''  # end string
-        return f'''Catching time:
-{self.catching_start}-{self.catching_end} | Coords:
-{self.Lat}:{self.Lon} | Pokemon: {self.title} |'''  # end string
+            return f'''| Pokemon:{self.pokedex_num} |'''  # end string
+        return f'''| Pokemon: {self.pokedex_num} |'''  # end string
+
+
+class Evolution(models.Model):
+    pokedex_num = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
+    next_evolution = models.IntegerField(blank=True, null=True)
+    previous_evolution = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        if not self.previous_evolution:
+            return f'| {self.pokedex_num} | to №{self.next_evolution} |'
+        if not self.next_evolution:
+            return f'| {self.pokedex_num} | from №{self.previous_evolution} |'
+        return f'| {self.pokedex_num} | from №{self.previous_evolution} | to №{self.next_evolution} |'
+
