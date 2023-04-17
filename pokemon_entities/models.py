8324dfa71f9578d72829e59ffa37b233b34d41b6
den1slx <keycoingold@mail.ru>
2023-04-18 04:26:02 +1000

    Update models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,22 +3,20 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title_en = models.CharField(max_length=200, blank=True, null=True, verbose_name='Английское название')
-    title_ru = models.CharField(max_length=200, blank=True, null=True, verbose_name='Русскаое название')
+    title_ru = models.CharField(max_length=200, null=True, verbose_name='Русское название')
     title_jp = models.CharField(max_length=200, blank=True, null=True, verbose_name='Японское название')
     pokedex_num = models.IntegerField(null=True, unique=True, verbose_name='id покемона в покедексе')
-    evolutions = models.ForeignKey(
+    previous_evolution = models.ForeignKey(
         'self',
-        verbose_name='Пердшествующая эволюция',
+        verbose_name='Предшествующая эволюция',
         null=True,
         blank=True,
-        related_name='next_evolution',
+        related_name='next_evolutions',
         on_delete=models.CASCADE,
     )
     description = models.TextField('Описание', blank=True, null=True)
     poke_image_local = models.ImageField(blank=True, null=True, verbose_name='Путь к картинке на вашем устройстве')
-    poke_image_link = models.URLField(
-        blank=True, null=True, verbose_name='Ссылка на картинку(используется если нет локальной)'
-    )
+
 
     def __str__(self):
         if self.pokedex_num and self.pokedex_num > 0:
@@ -29,11 +27,17 @@ class Pokemon(models.Model):
             if self.title_jp:
                 return f'№{self.pokedex_num} |  {self.title_jp}'
             return f'№{self.pokedex_num} |'
+        return f'№???? |  {self.title_ru}'
 
 
 
 class PokemonEntity(models.Model):
-    pokedex_num = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='id покемона в покедексе')
+    pokemon = models.ForeignKey(
+        Pokemon,
+        on_delete=models.CASCADE,
+        verbose_name='id покемона в покедексе',
+        related_name='Pokemons',
+    )
     latitude = models.FloatField(blank=True, null=True, verbose_name='Широта')
     longitude = models.FloatField(blank=True, null=True, verbose_name='Долгота')
     appeared_at = models.DateTimeField(null=True, blank=True, verbose_name='Появление покемона')
@@ -45,4 +49,4 @@ class PokemonEntity(models.Model):
     stamina = models.IntegerField(blank=True, null=True, verbose_name='Выносливость')
 
     def __str__(self):
-        return f'| Pokemon:{self.pokedex_num}'
+        return f'| Pokemon:{self.pokemon}'
